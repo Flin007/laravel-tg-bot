ba81f8087a9d1f8b2a015e8ffa1c1b6bcbb99b91
Vladimir <flin007@inbox.ru>
2023-03-12 12:54:21 +0300

    сохранение юзеров в базу данных

--- app/Commands/StartCommand.php
+++ app/Commands/StartCommand.php
@@ -2,7 +2,9 @@
 
 namespace App\Commands;
 
+use App\Models\TelegramUser;
 use Telegram\Bot\Commands\Command;
+use Telegram\Bot\Objects\User;
 
 class StartCommand extends Command
 {
@@ -11,8 +13,39 @@ class StartCommand extends Command
 
     public function handle()
     {
-        $this->replyWithMessage([
-           'text' => 'Привет мир'
+        $userData = $this->getUpdate()->message->from;
+        $user = $userData instanceof User ? $this->updateOrCreateTelegramUser($userData) : null;
+
+        if ($user){
+            $this->replyWithMessage([
+                'text' =>
+                    'Спасибо, я записал тебя в бд😈'
+                    . PHP_EOL . 'Твой тг ID - ' . $user->user_id . ','
+                    . PHP_EOL . 'Имя - ' . $user->first_name . ','
+                    . PHP_EOL . 'Фамилия - ' . $user->last_name . ','
+                    . PHP_EOL . 'Ник - ' . $user->username . ','
+                    . PHP_EOL . 'Язык - ' . $user->language_code . ','
+                    . PHP_EOL . 'Есть премиум? - ' . $user->is_premium . ','
+                    . PHP_EOL . 'Ты бот? - ' . $user->is_bot
+            ]);
+        }else{
+            $this->replyWithMessage([
+                'text' => 'Привет мир! Я не смог стырить твою инфу(('
+            ]);
+        }
+    }
+
+    public function updateOrCreateTelegramUser(User $userData)
+    {
+        return TelegramUser::updateOrCreate([
+           'user_id' => $userData->id,
+        ],[
+            'username' => $userData->username,
+            'first_name' => $userData->first_name,
+            'last_name' => $userData->last_name,
+            'language_code' => $userData->language_code,
+            'is_premium' => $userData->is_premium,
+            'is_bot' => $userData->is_bot,
         ]);
     }
 }
