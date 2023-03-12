5d9e93fb69c3b8b4ea0fd6b44da71c0380fbc61d
Vladimir <flin007@inbox.ru>
2023-03-12 12:53:55 +0300

    ide-helper + настройки модели тг юзеров

--- app/Models/TelegramUser.php
+++ app/Models/TelegramUser.php
@@ -5,7 +5,32 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\TelegramUser
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string|null $username
+ * @property string|null $first_name
+ * @property string|null $last_name
+ * @property string|null $language_code
+ * @property int|null $is_premium
+ * @method static \Illuminate\Database\Eloquent\Builder|TelegramUser newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|TelegramUser newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|TelegramUser query()
+ * @method static \Illuminate\Database\Eloquent\Builder|TelegramUser whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TelegramUser whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TelegramUser whereIsPremium($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TelegramUser whereLanguageCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TelegramUser whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TelegramUser whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|TelegramUser whereUsername($value)
+ * @mixin \Eloquent
+ */
 class TelegramUser extends Model
 {
     use HasFactory;
+
+    protected $table = 'telegram_users';
+    protected $guarded = false;
 }
